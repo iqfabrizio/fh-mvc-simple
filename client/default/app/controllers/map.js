bb1f35ed17419fba052bc7ff5119989edc37f020
keyang xiang <keyang.xiang@feedhenry.com>
2012-02-15 16:13:31 +0000

    impl

---
+++ client/default/app/controllers/map.js
@@ -0,0 +1,26 @@
+var map = {
+	pinCurrentLocation : function() {
+		var mapView=getView("map");
+		mapView.find("#status").html("Retriving your location");
+		$fh.geo({
+			interval : 0
+		}, function(res) {
+			mapView.find("#status").html("Loading map...");
+			mapView.find("#lat").text("res.lat");
+			mapView.find("#long").text("res.lon");
+			$fh.map({
+				target : '#map_div',
+				lat : res.lat,
+				lon : res.lon,
+				zoom : 9
+			}, function(map) {
+				mapView.find("#status").html("Done");
+			}, function(msg) {
+				console.log(msg);
+			});
+		})
+	},
+	back:function(){
+		changeView("logged");
+	}
+};
