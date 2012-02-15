835566b522838e16fdb566dd60664d6f6b9fa75c
keyang xiang <keyang.xiang@feedhenry.com>
2012-02-15 15:57:42 +0000

    development

--- client/default/app/models/user.js
+++ client/default/app/models/user.js
@@ -3,13 +3,20 @@
  */
 
 var users = {
-	data : [{
-		username : "Joe",
-		password : "12345"
-	}, {
-		username : "John",
-		password : "12345"
-	}],
+	data : [],
+	isDataLoaded:false,
+	load:function(callback){
+		var that=this;
+		$fh.act({
+			act:"getUsers"
+		},function(res){
+			that.data=res.data;
+			that.isDataLoaded=true;
+			if (callback){
+				callback()
+			}
+		});
+	},
 	userValidate : function(username, password, cb) {
 		var users = this.data;
 		setTimeout(function() {
---
+++ cloud/main.js
@@ -0,0 +1,12 @@
+var users = [{
+	username : "Joe",
+	password : "12345"
+}, {
+	username : "John",
+	password : "12345"
+}];
+
+
+function getUsers(){
+	return {data:users};
+}
