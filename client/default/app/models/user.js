9fd58ddc4fec08575404db3144cf643a4315d3d3
keyang xiang <keyang.xiang@feedhenry.com>
2012-02-16 11:35:22 +0000

    user list

--- client/default/app/models/user.js
+++ client/default/app/models/user.js
@@ -4,28 +4,28 @@
 
 var users = {
 	data : [],
-	isDataLoaded:false,
-	load:function(callback){
-		var that=this;
+	isDataLoaded : false,
+	load : function(callback) {
+		var that = this;
 		$fh.act({
-			act:"getUsers"
-		},function(res){
-			that.data=res.data;
-			that.isDataLoaded=true;
-			if (callback){
+			act : "getUsers"
+		}, function(res) {
+			that.data = res.data;
+			that.isDataLoaded = true;
+			if(callback) {
 				callback()
 			}
 		});
 	},
 	userValidate : function(username, password, cb) {
 		var users = this.data;
+		//unblock process
 		setTimeout(function() {
 			for(var i = 0; i < users.length; i++) {
 				var user = users[i];
 				if(user.username === username && user.password === password) {
 					if(cb != undefined) {
-						cb(true); //found specified user.
-						return;
+						cb(true);
 					}
 				}
 			}
@@ -33,5 +33,18 @@ var users = {
 				cb(false);
 			}
 		}, 100);
+	},
+	getUserList : function(callback) {
+		var retArr = [];
+		var users = this.data;
+		setTimeout(function() {
+			for(var i = 0; i < users.length; i++) {
+				var user = users[i];
+				retArr.push(user.username);
+			}
+			if (callback){
+				callback(retArr);
+			}
+		}, 100)
 	}
 };
